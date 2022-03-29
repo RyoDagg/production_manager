8b8806eddceb31624ef67e27d5eebe56206d422f
ryodagg <mannai.abdlkader@gmail.com>
2022-03-29 16:13:40 +0100

    Init Design

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index']);
 
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index']);
