d11216214175121573faf7bb5f85ae8758aeb7c7
ryodagg <mannai.abdlkader@gmail.com>
2022-04-11 14:40:30 +0100

    fixed Sale/purchases views

--- resources/views/tables/purchases.blade.php
+++ resources/views/tables/purchases.blade.php
@@ -78,7 +78,6 @@
         </div>
     </div>
 
-
     <div class="row layout-top-spacing" id="cancel-row">
         <div class="col-xl-12 col-lg-12 col-sm-12 layout-spacing">
             <div class="widget-content widget-content-area br-6">
@@ -126,7 +125,8 @@
                                             <select name="fournisseur" class="placeholder js-states form-control">
 
                                                 @foreach ($fournisseurs as $fournisseur)
-                                                    <option value="{{ $fournisseur->id }}">{{ $fournisseur->name }}</option>
+                                                    <option value="{{ $fournisseur->id }}">{{ $fournisseur->name }}
+                                                    </option>
                                                 @endforeach
                                             </select>
                                         </div>
@@ -148,107 +148,105 @@
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="table-responsive mb-4 mt-4">
-                <table id="range-search" class="display table table-hover" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th class="checkbox-column">
-                                {{-- <label class="new-control new-checkbox checkbox-primary" style="height: 18px; margin: 0 auto;">
+                <div class="table-responsive mb-4 mt-4">
+                    <table id="range-search" class="display table table-hover" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th class="checkbox-column">
+                                    {{-- <label class="new-control new-checkbox checkbox-primary" style="height: 18px; margin: 0 auto;">
                                     <input type="checkbox" class="new-control-input todochkbox" id="todoAll">
                                     <span class="new-control-indicator"></span>
                                 </label> --}}
-                            </th>
-                            <th>Date</th>
-                            <th>Material</th>
-                            <th>Supplier</th>
-                            <th>Quantity</th>
-                            <th>Unit Price</th>
-                            <th>Total Price</th>
-                            <th>Status</th>
-                            <th class="text-center"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $i = 0;
-                        ?>
-                        @foreach ($purchases as $purchase)
+                                </th>
+                                <th>Date</th>
+                                <th>Material</th>
+                                <th>Supplier</th>
+                                <th>Quantity</th>
+                                <th>Unit Price</th>
+                                <th>Total Price</th>
+                                <th>Status</th>
+                                <th class="text-center"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 0;
+                            ?>
+                            @foreach ($purchases as $purchase)
+                                <tr>
+                                    <td class="checkbox-column">
+                                        <label class="new-control new-checkbox checkbox-primary"
+                                            style="height: 18px; margin: 0 auto;">
+                                            <input type="checkbox" class="new-control-input todochkbox" id="todo-1">
+                                            <span class="new-control-indicator"></span>
+                                        </label>
+                                    </td>
+                                    <td>{{ date('d-m-y', strtotime($purchase->created_at)) }}</td>
+                                    <td>{{ $purchases[$i]->fournisseurs->name }}</td>
+                                    <td>{{ $purchases[$i]->materials->name }}</td>
+                                    <td>{{ $purchases[$i]->quantity }}</td>
+                                    <td> {{ $purchases[$i]->prix_unit }}</td>
+                                    <td>{{ $purchases[$i]->prix_tot }}</td>
+                                    <td>{{ $purchases[$i]->status }}</td>
+                                    <td class="text-center">
+                                        <ul class="table-controls">
+                                            <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
+                                                    title="Shop"><img src="icons/cart.png" width="25" height="25"
+                                                        alt=""></a>
+                                            </li>
+                                            <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
+                                                    title="View"><img src="icons/view.png" width="25" height="25"
+                                                        alt=""></a>
+                                            </li>
+                                            <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
+                                                    title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-edit-2 text-success">
+                                                        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
+                                                        </path>
+                                                    </svg></a></li>
+                                            <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
+                                                    title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-trash-2 text-danger">
+                                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                                        <path
+                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                        </path>
+                                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                                    </svg></a></li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                                <?php
+                                $i++;
+                                ?>
+                            @endforeach
                             <tr>
-                                <td class="checkbox-column">
+                                <th class="checkbox-column">
                                     <label class="new-control new-checkbox checkbox-primary"
                                         style="height: 18px; margin: 0 auto;">
-                                        <input type="checkbox" class="new-control-input todochkbox" id="todo-1">
+                                        <input type="checkbox" class="new-control-input todochkbox" id="todoAll">
                                         <span class="new-control-indicator"></span>
                                     </label>
-                                </td>
-                                <td>{{ date('d-m-y', strtotime($purchase->created_at)) }}</td>
-                                <td>{{ $purchases[$i]->fournisseurs->name }}</td>
-                                <td>{{ $purchases[$i]->materials->name }}</td>
-                                <td>{{ $purchases[$i]->quantity }}</td>
-                                <td> {{ $purchases[$i]->prix_unit }}</td>
-                                <td>{{ $purchases[$i]->prix_tot }}</td>
-                                <td>{{ $purchases[$i]->status }}</td>
-                                <td class="text-center">
-                                    <ul class="table-controls">
-                                        <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
-                                                title="Shop"><img src="icons/cart.png" width="25" height="25" alt=""></a>
-                                        </li>
-                                        <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
-                                                title="View"><img src="icons/view.png" width="25" height="25" alt=""></a>
-                                        </li>
-                                        <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
-                                                title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                    stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-edit-2 text-success">
-                                                    <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
-                                                    </path>
-                                                </svg></a></li>
-                                        <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top"
-                                                title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-trash-2 text-danger">
-                                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                                    <path
-                                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                    </path>
-                                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                                </svg></a></li>
-                                    </ul>
-                                </td>
+                                </th>
+                                <th>Date</th>
+                                <th>Material</th>
+                                <th>Supplier</th>
+                                <th>Quantity</th>
+                                <th>Unit Price</th>
+                                <th>Total Price</th>
+                                <th>Status</th>
+                                <th class="text-center"></th>
                             </tr>
-                            <?php
-                            $i++;
-                            ?>
-                        @endforeach
-                        <tr>
-                            <th class="checkbox-column">
-                                <label class="new-control new-checkbox checkbox-primary"
-                                    style="height: 18px; margin: 0 auto;">
-                                    <input type="checkbox" class="new-control-input todochkbox" id="todoAll">
-                                    <span class="new-control-indicator"></span>
-                                </label>
-                            </th>
-                            <th>Date</th>
-                            <th>Material</th>
-                            <th>Supplier</th>
-                            <th>Quantity</th>
-                            <th>Unit Price</th>
-                            <th>Total Price</th>
-                            <th>Status</th>
-                            <th class="text-center"></th>
-                        </tr>
-                        </tfoot>
-                </table>
-
-
+                            </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-
     </div>
 @endsection
